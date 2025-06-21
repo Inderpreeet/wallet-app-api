a05981e0b5dd443a386e09093e31039e525652d7
Inderpreeet <A00291274@mycambrian.ca>
2025-06-20 22:30:49 -0400

    Updated Class1.cs with BudgetCalculator logic

--- WalletApp.Library/Class1.cs
+++ WalletApp.Library/Class1.cs
@@ -1,6 +1,15 @@
-﻿namespace WalletApp.Library;
-
-public class Class1
+﻿namespace WalletApp.Library.Services
 {
+    public class BudgetCalculator
+    {
+        public decimal CalculateRemainingBudget(decimal totalBudget, decimal expenses)
+        {
+            if (expenses < 0 || totalBudget < 0)
+            {
+                throw new ArgumentException("Values must be non-negative");
+            }
 
+            return totalBudget - expenses;
+        }
+    }
 }
