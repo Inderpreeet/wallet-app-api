633207b0cf0d9ac8cc3d002cc5d947679b758dcf
Inderpreeet <A00291274@mycambrian.ca>
2025-06-20 22:32:56 -0400

    Added unit tests for BudgetCalculator in WalletApp.Tests

--- WalletApp.Tests/UnitTest1.cs
+++ WalletApp.Tests/UnitTest1.cs
@@ -1,10 +1,27 @@
-﻿namespace WalletApp.Tests;
+﻿using WalletApp.Library.Services;
+using Xunit;
 
-public class UnitTest1
+namespace WalletApp.Tests
 {
-    [Fact]
-    public void Test1()
+    public class BudgetCalculatorTests
     {
+        [Fact]
+        public void CalculateRemainingBudget_ReturnsCorrectValue()
+        {
+            var calculator = new BudgetCalculator();
+            var result = calculator.CalculateRemainingBudget(1000m, 400m);
 
+            Assert.Equal(600m, result);
+        }
+
+        [Fact]
+        public void CalculateRemainingBudget_ThrowsOnNegativeInputs()
+        {
+            var calculator = new BudgetCalculator();
+
+            Assert.Throws<ArgumentException>(() => calculator.CalculateRemainingBudget(-100m, 50m));
+            Assert.Throws<ArgumentException>(() => calculator.CalculateRemainingBudget(100m, -50m));
+        }
     }
 }
+
